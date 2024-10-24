a30c8fc08063428f820e44664c5153914cc37f77
Khaled Hossameldin <khaled-hossam@outlook.com>
2024-10-24 14:19:42 +0300

    add join event to events remote data source

--- lib/features/events/data/data_sources/remote/events_remote_data_source.dart
+++ lib/features/events/data/data_sources/remote/events_remote_data_source.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 import '../../models/event_model.dart';
@@ -12,8 +14,15 @@ class EventsRemoteDataSourceImpl implements EventsRemoteDataSource {
         .map((event) => EventModel.fromJson(event.data()))
         .toList();
   }
+
+  @override
+  Future<bool> joinEvent() async {
+    await Future.delayed(const Duration(seconds: 2));
+    return Random().nextBool();
+  }
 }
 
 abstract class EventsRemoteDataSource {
   Future<List<EventModel>> getEvents();
+  Future<bool> joinEvent();
 }
