d059a925c0defdd43c086d00a83c6a5e042e7b1f
Khaled Hossameldin <khaled-hossam@outlook.com>
2024-10-24 14:17:08 +0300

    add event details screen ui

--- lib/features/events/presentation/screens/event_details_screen.dart
+++ lib/features/events/presentation/screens/event_details_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:intl/intl.dart';
 
 import '../../domain/entities/event.dart';
 
@@ -7,11 +9,45 @@ class EventDetailsScreen extends StatelessWidget {
   static const route = '/details';
   final Event event;
 
+  void _join() {}
+
   @override
   Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+
     return Scaffold(
       appBar: AppBar(title: const Text('Event Details')),
-      body: Center(child: Text(event.name)),
+      body: Container(
+        width: double.infinity,
+        padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 16.h),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text.rich(
+              TextSpan(children: [
+                const TextSpan(
+                  text: 'Name: ',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                TextSpan(text: event.name),
+              ]),
+              style: textTheme.titleLarge,
+            ),
+            8.verticalSpace,
+            Text(event.details, style: textTheme.bodyLarge),
+            16.verticalSpace,
+            Text(event.location, style: textTheme.bodyLarge),
+            16.verticalSpace,
+            Text(
+              DateFormat.yMMMMEEEEd().format(event.time),
+              style: textTheme.bodyLarge,
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: ElevatedButton(onPressed: _join, child: const Text('Join')),
+      ),
     );
   }
 }
