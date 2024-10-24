896c41654e7d832a2e0231853d4e27b11cb45c55
Khaled Hossameldin <khaled-hossam@outlook.com>
2024-10-24 11:56:51 +0300

    add firebase

---
+++ lib/features/events/presentation/screens/events_list_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class EventsListScreen extends StatelessWidget {
+  const EventsListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Events')),
+    );
+  }
+}
